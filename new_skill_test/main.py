83195bacb579d39a172ec923d5ff2c4c42ae13a6
Jay Hotta <fukuzo@cybertron.co.jp>
2016-05-28 07:06:34 +0900

    new skill test dir added for the 1st test

---
+++ new_skill_test/main.py
@@ -0,0 +1,23 @@
+import logging
+from AlexaHandler import AlexaDeploymentTestHandler
+
+#  Main entry point for the Lambda function.
+#  In the AWS Lamba console, under the 'Configuration' tab there is an
+#  input field called, 'Handler'.  That should be:  main.lambda_handler
+
+#  Handler: main.lambda_handler
+#  Role: lambda_basic_execution
+
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    logging.info("Executing main lambda_handler for AlexaDeploymentTestHandler class")
+
+    deployment_handler = AlexaDeploymentTestHandler()
+    handler_response = deployment_handler.process_request(event, context)
+
+    return handler_response
